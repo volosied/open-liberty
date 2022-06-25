d014b0ac83c55f1dea1ead896fc5e5f9e4f4acd8
Volodymyr Siedlecki <volosied@gmail.com>
2022-06-24 23:12:45 -0400

    Create com.ibm.ws.wsoc.1.0.internal

---
+++ dev/com.ibm.ws.wsoc.1.0.internal/src/com/ibm/ws/wsoc/servercontainer10/ServerContainerImplFactory10.java
@@ -0,0 +1,27 @@
+/*******************************************************************************
+ * Copyright (c) 2022 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package com.ibm.ws.wsoc.servercontainer10;
+
+import com.ibm.ws.wsoc.servercontainer.ServerContainerExt;
+import com.ibm.ws.wsoc.servercontainer.ServletContainerFactory;
+import org.osgi.service.component.annotations.Component;
+
+@Component(property = { "service.vendor=IBM" })
+public class ServerContainerImplFactory10 implements ServletContainerFactory {
+
+    private static final ServerContainerExt10 serverContainerExt10 = new ServerContainerExt10();
+
+    @Override
+    public ServerContainerExt getServletContainer() {
+        return serverContainerExt10;
+    }
+
+}
