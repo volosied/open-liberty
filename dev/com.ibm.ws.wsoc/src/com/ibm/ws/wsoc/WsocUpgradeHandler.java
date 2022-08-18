4f82e65038ad5c8449d4626c35825b975fb72981
Volodymyr Siedlecki <volosied@gmail.com>
2022-08-18 16:36:07 -0400

    remove sys.out print

--- dev/com.ibm.ws.wsoc/src/com/ibm/ws/wsoc/WsocUpgradeHandler.java
+++ dev/com.ibm.ws.wsoc/src/com/ibm/ws/wsoc/WsocUpgradeHandler.java
@@ -76,8 +76,6 @@ public class WsocUpgradeHandler implements HttpUpgradeHandler, TransportConnecti
         sessionImpl.setPathParameters();
         sessionImpl.signalAppOnOpen();
 
-        System.out.println(WebSocketVersionServiceManager.LOADED_SPEC_LEVEL);
-
         // release ref to the endpoint
         endpoint = null;
     }
