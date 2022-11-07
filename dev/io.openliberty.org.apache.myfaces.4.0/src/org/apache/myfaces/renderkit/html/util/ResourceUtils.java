3a80802d4fea3a2857b7ed31f1cedcc3f801f694
Volodymyr Siedlecki <volosied@gmail.com>
2022-11-07 09:36:14 -0500

    Add java sources

---
+++ dev/io.openliberty.org.apache.myfaces.4.0/src/org/apache/myfaces/renderkit/html/util/ResourceUtils.java
@@ -0,0 +1,150 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.myfaces.renderkit.html.util;
+
+import java.io.IOException;
+import jakarta.faces.FacesWrapper;
+
+import jakarta.faces.application.Resource;
+import jakarta.faces.application.ResourceHandler;
+import jakarta.faces.context.FacesContext;
+import jakarta.faces.context.PartialViewContext;
+import jakarta.faces.context.ResponseWriter;
+
+import org.apache.myfaces.resource.ContractResource;
+
+public class ResourceUtils
+{
+    public final static String MYFACES_LIBRARY_NAME = "org.apache.myfaces";
+
+    public final static String JSF_MYFACES_JSFJS_MINIMAL = "minimal";
+    public final static String JSF_MYFACES_JSFJS_NORMAL = "normal";
+    
+    public final static String FACES_UNCOMPRESSED_JS_RESOURCE_NAME = "faces-uncompressed.js";
+    public final static String FACES_UNCOMPRESSED_FULL_JS_RESOURCE_NAME = "faces-uncompressed-full.js";
+    public final static String FACES_MINIMAL_JS_RESOURCE_NAME = "faces-minimal.js";
+    public final static String FACES_MYFACES_JSFJS_I18N = "faces-i18n.js";
+
+    private final static String RENDERED_FACES_JS = "org.apache.myfaces.RENDERED_FACES_JS";
+
+    public static final String JAKARTA_FACES_OUTPUT_COMPONENT_TYPE = "jakarta.faces.Output";
+    public static final String JAKARTA_FACES_TEXT_RENDERER_TYPE = "jakarta.faces.Text";
+    public static final String DEFAULT_SCRIPT_RENDERER_TYPE = "jakarta.faces.resource.Script";
+    public static final String DEFAULT_STYLESHEET_RENDERER_TYPE = "jakarta.faces.resource.Stylesheet";
+
+    public static void markScriptAsRendered(FacesContext facesContext, String libraryName, String resourceName)
+    {
+        facesContext.getApplication().getResourceHandler().markResourceRendered(
+                facesContext, resourceName, libraryName);
+    }
+    
+    public static void markStylesheetAsRendered(FacesContext facesContext, String libraryName, String resourceName)
+    {
+        facesContext.getApplication().getResourceHandler().markResourceRendered(
+                facesContext, resourceName, libraryName);
+    }
+    
+    public static boolean isRenderedScript(FacesContext facesContext, String libraryName, String resourceName)
+    {
+        return facesContext.getApplication().getResourceHandler().isResourceRendered(
+                facesContext, resourceName, libraryName);
+    }
+    
+    public static boolean isRenderedStylesheet(FacesContext facesContext, String libraryName, String resourceName)
+    {
+        return facesContext.getApplication().getResourceHandler().isResourceRendered(
+                facesContext, resourceName, libraryName);
+    }
+    
+    public static void writeScriptInline(FacesContext facesContext, ResponseWriter writer, String libraryName, 
+            String resourceName) throws IOException
+    {
+        if (!ResourceUtils.isRenderedScript(facesContext, libraryName, resourceName))
+        {
+            //Fast shortcut, don't create component instance and do what HtmlScriptRenderer do.
+            Resource resource = facesContext.getApplication().getResourceHandler().createResource(
+                    resourceName, libraryName);
+            markScriptAsRendered(facesContext, libraryName, resourceName);
+            writer.startElement(HTML.SCRIPT_ELEM, null);
+            HtmlRendererUtils.renderScriptType(facesContext, writer);
+            writer.writeURIAttribute(HTML.SRC_ATTR, resource.getRequestPath(), null);
+            writer.endElement(HTML.SCRIPT_ELEM);
+        }
+    }
+    
+    public static void renderDefaultJsfJsInlineIfNecessary(FacesContext facesContext, ResponseWriter writer) 
+        throws IOException
+    {
+        if (facesContext.getAttributes().containsKey(RENDERED_FACES_JS))
+        {
+            return;
+        }
+        
+        // Check first if we have lucky, we are using myfaces and the script has
+        // been previously rendered
+        if (isRenderedScript(facesContext, ResourceHandler.FACES_SCRIPT_LIBRARY_NAME,
+                ResourceHandler.FACES_SCRIPT_RESOURCE_NAME))
+        {
+            facesContext.getAttributes().put(RENDERED_FACES_JS, Boolean.TRUE);
+            return;
+        }
+
+        // Check if this is an ajax request. If so, we don't need to include it, because that was
+        // already done and in the worst case, jsf script was already loaded on the page.
+        PartialViewContext partialViewContext = facesContext.getPartialViewContext();
+        if (partialViewContext != null && 
+                (partialViewContext.isPartialRequest() || partialViewContext.isAjaxRequest()))
+        {
+            return;
+        }
+
+        //Fast shortcut, don't create component instance and do what HtmlScriptRenderer do.
+        Resource resource = facesContext.getApplication().getResourceHandler().createResource(
+                ResourceHandler.FACES_SCRIPT_RESOURCE_NAME, ResourceHandler.FACES_SCRIPT_LIBRARY_NAME);
+        markScriptAsRendered(facesContext, ResourceHandler.FACES_SCRIPT_LIBRARY_NAME,
+                ResourceHandler.FACES_SCRIPT_RESOURCE_NAME);
+        writer.startElement(HTML.SCRIPT_ELEM, null);
+        HtmlRendererUtils.renderScriptType(facesContext, writer);
+        writer.writeURIAttribute(HTML.SRC_ATTR, resource.getRequestPath(), null);
+        writer.endElement(HTML.SCRIPT_ELEM);
+
+        //mark as rendered
+        facesContext.getAttributes().put(RENDERED_FACES_JS, Boolean.TRUE);
+    }
+
+    public static String getContractName(Resource resource)
+    {
+        while (resource != null)
+        {
+            if (resource instanceof ContractResource)
+            {
+                return ((ContractResource)resource).getContractName();
+            }
+            else if (resource instanceof FacesWrapper)
+            {
+                resource = (Resource) ((FacesWrapper)resource).getWrapped();
+            }
+            else
+            {
+                resource = null;
+            }
+        }
+        return null;
+    }
+}
---
+++ dev/io.openliberty.org.apache.myfaces.4.0/src/org/apache/myfaces/util/ExternalContextUtils.java
@@ -0,0 +1,145 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.myfaces.util;
+
+import java.util.Map;
+import org.apache.myfaces.util.lang.ClassUtils;
+
+
+import jakarta.faces.context.ExternalContext;
+import jakarta.servlet.ServletResponseWrapper;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ * This provides some functionality for determining some things about the
+ * native request object that is not provided by Faces.  This class is useful
+ * for use in places where Portlet API's may or may not be present and can
+ * also provide access to some request-specific items which are not available on
+ * the Faces ExternalContext.  If portlet API's are not present, this class simply 
+ * handles the Servlet Request type.
+ */
+public final class ExternalContextUtils
+{
+    private static final Class<?> PORTLET_CONTEXT_CLASS =
+            ClassUtils.simpleClassForName("javax.portlet.PortletContext", false);
+
+    /**
+     * Constant defined on javax.portlet.faces.Bridge class that helps to 
+     * define if the current request is a portlet request or not.
+     */
+    private static final String PORTLET_LIFECYCLE_PHASE = "javax.portlet.faces.phase";    
+    
+    /**
+     * This is a convenience function designed to perform a quick check of the current {@link ExternalContext}.
+     * 
+     * @param ec the current external context
+     * @return <code>true</code> if the current {@link ExternalContext} is a Porlet.
+     */
+    public static boolean isPortlet(ExternalContext ec)
+    {
+        if (PORTLET_CONTEXT_CLASS == null)
+        {
+            return false;
+        }
+        
+        if (PORTLET_CONTEXT_CLASS.isInstance(ec.getContext()))
+        {
+            return true;
+        }
+        
+        Map<String, Object> requestMap = ec.getRequestMap();
+        if (requestMap.containsKey(PORTLET_LIFECYCLE_PHASE))
+        {
+            return true;
+        }
+        
+        return false;
+    }
+
+    /**
+     * Returns wherther of not this external context represents a true HttpServletRequest or
+     * not.  Some portal containers implement the PortletRequest/Response objects as 
+     * HttpServletRequestWrappers, and those objects should not be treated as an
+     * HttpServlerRequest.  As such, this method first tests to see if the request is
+     * a portlet request and, if not, then tests to see if the request is an instanceof
+     * HttpServletRequest.
+     * 
+     * @param ec the current external context
+     * @return a boolean value of <code>true</code> if the current request is an
+     *         HttpServletRequest
+     */
+    public static boolean isHttpServletRequest(ExternalContext ec)
+    {
+        return (!isPortlet(ec) && (ec.getRequest() instanceof HttpServletRequest));
+    }
+
+    /**
+     * Returns an HttpServletResponse if one exists on the externalContext or null
+     * if it does not.  Please note that some portal environments implement the
+     * PortletRequest and Response objects as HttpServletRequest/Response objects.
+     * This method handles these types of requests properly and will therefore
+     * return null in portal environments.
+     * 
+     * @param ec
+     * @return an HttpServletResponse if we have one or null if we do not
+     */
+    public static HttpServletResponse getHttpServletResponse(ExternalContext ec)
+    {
+        if (isHttpServletRequest(ec))
+        {
+            return (HttpServletResponse) ec.getResponse();
+        }
+
+        return null;
+    }
+    
+    /**
+     * Trys to obtain a HttpServletResponse from the Response.
+     * Note that this method also trys to unwrap any ServletResponseWrapper
+     * in order to retrieve a valid HttpServletResponse.
+     * @param response
+     * @return if found, the HttpServletResponse, null otherwise
+     */
+    public static HttpServletResponse getHttpServletResponse(Object response)
+    {
+        // unwrap the response until we find a HttpServletResponse
+        while (response != null)
+        {
+            if (response instanceof HttpServletResponse)
+            {
+                // found
+                return (HttpServletResponse) response;
+            }
+            if (response instanceof ServletResponseWrapper)
+            {
+                // unwrap
+                response = ((ServletResponseWrapper) response).getResponse();
+            }
+            // no more possibilities to find a HttpServletResponse
+            break;
+        }
+        return null; // not found
+    }
+
+    // prevent this from being instantiated
+    private ExternalContextUtils()
+    {
+    }
+}
