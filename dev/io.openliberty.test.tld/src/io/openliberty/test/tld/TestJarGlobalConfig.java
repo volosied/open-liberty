10dd41eeea9d3212bbb293427e403fa5c24b014d
Volodymyr Siedlecki <volosied@gmail.com>
2023-10-16 20:15:41 -0400

    Try to get GlobalTagLibConfig to work with a jar

--- dev/io.openliberty.test.tld/src/io/openliberty/test/tld/TestJarGlobalConfig.java
+++ dev/io.openliberty.test.tld/src/io/openliberty/test/tld/TestJarGlobalConfig.java
@@ -7,26 +7,26 @@ import java.security.PrivilegedAction;
 import com.ibm.wsspi.jsp.taglib.config.GlobalTagLibConfig;
 import com.ibm.wsspi.jsp.taglib.config.TldPathConfig;
 
-public class TestGlobalConfig extends GlobalTagLibConfig {
-    public TestGlobalConfig() {
+public class TestJarGlobalConfig extends GlobalTagLibConfig {
+    public TestJarGlobalConfig() {
         super();
 
-        setJarName("test-tld.jar");
+        setJarName("tld2-sample.jar");
 
 
-        addtoTldPathList(new TldPathConfig("WEB-INF/tld/test-1.tld", "io.tld", null));
+        addtoTldPathList(new TldPathConfig("META-INF/f.tld", "http://sastruts.seasar.org/functions", null));
 
         
 
         setClassloader(AccessController.doPrivileged(new PrivilegedAction<ClassLoader>() {
             public ClassLoader run() {
-                return TestGlobalConfig.class.getClassLoader();
+                return TestJarGlobalConfig.class.getClassLoader();
             }
         }));
 
         setJarURL(AccessController.doPrivileged(new PrivilegedAction<URL>() {
             public URL run() {
-                return getClassloader().getResource("WEB-INF/tld/test-1.tld");
+                return getClassloader().getResource("META-INF/tld2-sample.jar");
             }
         }));
     }
