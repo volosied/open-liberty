11453d208c6ce9cbd7513ab636f2eb39bdfe4dc6
Volodymyr Siedlecki <volosied@gmail.com>
2024-09-13 08:58:04 -0400

    Unminify sort of

--- dev/com.ibm.websphere.javaee.jsf.2.2/src/META-INF/resources/myfaces/META-INF/resources/myfaces/_impl/xhrCore/_ExtAjaxRequest.js
+++ dev/com.ibm.websphere.javaee.jsf.2.2/src/META-INF/resources/myfaces/META-INF/resources/myfaces/_impl/xhrCore/_ExtAjaxRequest.js
@@ -10,4 +10,12 @@
  * Contributors:
  *     IBM Corporation - initial API and implementation
  *******************************************************************************/
-myfaces._impl.xhrCore._AjaxRequest=_MF_CLS(_PFX_XHR+"_ExtAjaxRequest",myfaces._impl.xhrCore._AjaxRequest,{constructor_:function(A){this._callSuper("constructor_",A);},getFormData:function(){var C=this._AJAXUTIL,A=this._context.myfaces,B=null;if(!this._partialIdsArray||!this._partialIdsArray.length){B=this._callSuper("getFormData");if(this._source&&!this._isBehaviorEvent()){C.appendIssuingItem(this._source,B);}}else{B=this._Lang.createFormDataDecorator(new Array());C.encodeSubmittableFields(B,this._sourceForm,this._partialIdsArray);if(this._source&&!this._isBehaviorEvent()){C.appendIssuingItem(this._source,B);}}return B;}});
+myfaces._impl.xhrCore._AjaxRequest=_MF_CLS(_PFX_XHR+"_ExtAjaxRequest",myfaces._impl.xhrCore._AjaxRequest,{constructor_:function(A){this._callSuper("constructor_",A);
+},getFormData:function(){var C=this._AJAXUTIL,A=this._context.myfaces,B=null;
+if(!this._partialIdsArray||!this._partialIdsArray.length){B=this._callSuper("getFormData");
+if(this._source&&!this._isBehaviorEvent()){C.appendIssuingItem(this._source,B);
+}}else{B=this._Lang.createFormDataDecorator(new Array());
+C.encodeSubmittableFields(B,this._sourceForm,this._partialIdsArray);
+if(this._source&&!this._isBehaviorEvent()){C.appendIssuingItem(this._source,B);
+}}return B;
+}});
