05c5a5adbe94b2656eab4f1072d3c31070b02cd2
Volodymyr Siedlecki <volosied@gmail.com>
2025-04-09 15:26:01 -0400

    Update #5

--- dev/io.openliberty.checkpoint_fat_faces/fat/src/io/openliberty/checkpoint/fat/FATSuite.java
+++ dev/io.openliberty.checkpoint_fat_faces/fat/src/io/openliberty/checkpoint/fat/FATSuite.java
@@ -15,12 +15,16 @@ package io.openliberty.checkpoint.fat;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.OutputStream;
+import java.util.Locale;
 import java.util.Map;
 import java.util.Properties;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
+
+import com.ibm.ws.fat.util.FatLogHandler;
+import componenttest.custom.junit.runner.FATRunner;
 
 import componenttest.custom.junit.runner.AlwaysPassesTest;
 import componenttest.topology.impl.LibertyServer;
@@ -34,5 +38,37 @@ import componenttest.topology.impl.LibertyServer;
 })
 
 public class FATSuite {
+    @ClassRule
+    public static RepeatTests repeat;
 
+    private static final boolean isWindows = System.getProperty("os.name").toLowerCase(Locale.ENGLISH).contains("win");
+
+    static {
+        // EE10 requires Java 11.
+        // EE11 requires Java 17
+        // If we only specify EE10/EE11 for lite mode it will cause no tests to run with lower Java versions which causes an error.
+        if (isWindows && !FATRunner.FAT_TEST_LOCALRUN) {
+            // Repeating the full fat for all features may exceed the 3 hour limit on Fyre Windows and causes random build breaks.
+            // Skip EE9 on the windows platform when not running locally.
+            // If we are running with a Java version less than 11, have EE8 (EmptyAction) be the lite mode test to run.
+            repeat = RepeatTests.with(new EmptyAction().conditionalFullFATOnly(EmptyAction.GREATER_THAN_OR_EQUAL_JAVA_11))
+                            .andWith(FeatureReplacementAction.EE10_FEATURES().conditionalFullFATOnly(FeatureReplacementAction.GREATER_THAN_OR_EQUAL_JAVA_17))
+                            .andWith(FeatureReplacementAction.EE11_FEATURES());
+        } else {
+            // If we are running with a Java version less than 11, have EE9 be the lite mode test to run.
+            repeat = RepeatTests.with(new EmptyAction().fullFATOnly())
+                            .andWith(FeatureReplacementAction.EE9_FEATURES().conditionalFullFATOnly(FeatureReplacementAction.GREATER_THAN_OR_EQUAL_JAVA_11))
+                            .andWith(FeatureReplacementAction.EE10_FEATURES().conditionalFullFATOnly(FeatureReplacementAction.GREATER_THAN_OR_EQUAL_JAVA_17))
+                            .andWith(FeatureReplacementAction.EE11_FEATURES());
+        }
+    }
+
+    /**
+     * @see {@link FatLogHandler#generateHelpFile()}
+     */
+    @BeforeClass
+    public static void generateHelpFile() {
+        FatLogHandler.generateHelpFile();
+    }
+}
 }
